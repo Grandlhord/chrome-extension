d638519ae808a18e6121323607d58e07a03c6c48
Wisdom Eli <104230238+Grandlhord@users.noreply.github.com>
2023-07-04 13:23:55 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let myLeads=[]
+
+const inputEl = document.getElementById("input-el")
+const inputbtn=document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
+
+
+inputbtn.addEventListener("click", function(){
+   myLeads.push(inputEl.value)
+   inputEl.value=""
+   renderLeads()
+})
+
+ function renderLeads(){
+    let listItems=" "
+    for(let i=0; i<myLeads.length; i++){
+        listItems += `
+         <li>
+             <a target='_blank' href='${myLeads[i]}'>
+                 ${myLeads[i]}
+             </a>
+        </li>`
+    }
+        ulEl.innerHTML=listItems
+ }
+
